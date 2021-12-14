6333a3cfbf974c358aa53dbacf161538c9e76b17
Komol <tegirmon@gmail.com>
2021-12-13 21:49:16 -0500

    update colors

--- app/src/main/java/com/quran/audio/app/ui/theme/Color.kt
+++ app/src/main/java/com/quran/audio/app/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.quran.audio.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Blue200 = Color(0xFF448AFF)
+val Blue400 = Color(0xFF2979FF)
+val Blue700 = Color(0xFF2962FF)
+val Orange200 = Color(0xFFFFAB40)
